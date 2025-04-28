c4a24c3cef20ab229829e473868c98668417a202
Haley <huanghongweimax@163.com>
2025-04-28 18:44:38 +0800

    adjust structure

--- backend/config/config.go
+++ backend/config/config.go
@@ -7,13 +7,6 @@ import (
 	"path/filepath"
 )
 
-type MySQLConfig struct {
-	HOST     string `mapstructure:"host"`
-	PORT     string `mapstructure:"port"`
-	USER     string `mapstructure:"username"`
-	PASSWORD string `mapstructure:"password"`
-	DBNAME   string `mapstructure:"dbname"`
-}
 type Config struct {
 	MYSQL MySQLConfig `mapstructure:"mysql"`
 }
