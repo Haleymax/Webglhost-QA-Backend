dc7751f2cd980710fbaf575d7d0e4664f2f15e87
mac-haley <hongwei.huang42@gmail.com>
2025-05-24 23:25:26 +0800

    add mongodb

---
+++ backend/internal/app/models/phone.go
@@ -0,0 +1,6 @@
+package models
+
+type Phone struct {
+	Brand  string `json:"brand"`
+	Serial string `json:"serial"`
+}
--- backend/internal/app/repositories/phone_repository.go
+++ backend/internal/app/repositories/phone_repository.go
@@ -1,1 +1,20 @@
 package repositories
+
+import (
+	"github.com/Webglhost-QA-Backend/backend/internal/app/models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type PhoneRepository interface {
+	InsertOne(phone *models.Phone) error
+}
+
+type PhoneRepositoryImpl struct {
+	mongo *mongo.Client
+}
+
+func NewPhoneRepository(mongo *mongo.Client) *PhoneRepositoryImpl {
+	return &PhoneRepositoryImpl{
+		mongo: mongo,
+	}
+}
