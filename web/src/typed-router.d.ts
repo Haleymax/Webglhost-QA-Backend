08383cf924ecb50568fe61f928c035a2b0c881d9
mac-haley <hongwei.huang42@gmail.com>
2025-05-27 22:43:39 +0800

    add watcher page

--- web/src/typed-router.d.ts
+++ web/src/typed-router.d.ts
@@ -23,5 +23,6 @@ declare module 'vue-router/auto-routes' {
     '/device/device_manage': RouteRecordInfo<'/device/device_manage', '/device/device_manage', Record<never, never>, Record<never, never>>,
     '/device/upload': RouteRecordInfo<'/device/upload', '/device/upload', Record<never, never>, Record<never, never>>,
     '/home/home': RouteRecordInfo<'/home/home', '/home/home', Record<never, never>, Record<never, never>>,
+    '/watcher/watcher_manage': RouteRecordInfo<'/watcher/watcher_manage', '/watcher/watcher_manage', Record<never, never>, Record<never, never>>,
   }
 }
