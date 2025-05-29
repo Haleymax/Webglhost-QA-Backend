1b18f731acf12e49a27812d247aec225d76fe940
Haley <huanghongweimax@163.com>
2025-05-29 16:29:51 +0800

    add new dependencies

--- web/src/components.d.ts
+++ web/src/components.d.ts
@@ -13,5 +13,6 @@ declare module 'vue' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Updata_node: typeof import('./components/node/updata_node.vue')['default']
+    Update_watcher: typeof import('./components/watcher/update_watcher.vue')['default']
   }
 }
