5fd3a9726b3b1919fca515636c0a2a42cc702172
Haley <huanghongweimax@163.com>
2025-05-30 16:33:33 +0800

    finished refreshing cache

--- backend/pkg/cache_client/redis.go
+++ backend/pkg/cache_client/redis.go
@@ -5,15 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/Webglhost-QA-Backend/backend/config"
+	"github.com/Webglhost-QA-Backend/backend/internal/app/models"
 	"github.com/redis/go-redis/v9"
 	"log"
 	"time"
 )
-
-type WatcherRequest struct {
-	Env     string `json:"env"`
-	Runtime string `json:"runtime"`
-}
 
 type WatcherCache struct {
 	Brand    string
@@ -95,4 +91,41 @@ func (r *Redis) SetKey(space, env, runtime string, watcher WatcherCache) error {
 	value_json_str, _ := json.Marshal(value)
 	_, err := r.Client.Set(ctx, pattern, value_json_str, 0).Result()
 	return err
+}
+
+func Get_all_brand_from_watcher(watchers []*models.Watcher) []string {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("error：%v\n", r)
+		}
+	}()
+	var brands []string
+	var brands_map = make(map[string]bool)
+	for _, watcher := range watchers {
+		for _, bds := range watcher.Brand {
+			brands_map[bds] = true
+		}
+	}
+	for key := range brands_map {
+		brands = append(brands, key)
+	}
+
+	return brands
+}
+
+func Convert(watchers []models.Watcher, brand, tag string) []WatcherCache {
+	/*
+		将数据库中的转换为方便插入到redis中的格式
+	*/
+	result := make([]WatcherCache, len(watchers))
+	for _, watcher := range watchers {
+		data := new(WatcherCache)
+		data.Brand = brand
+		data.Name = watcher.Name
+		data.Resource = watcher.Resource
+		data.Event = watcher.Click
+		data.Tag = tag
+		result = append(result, *data)
+	}
+	return result
 }
